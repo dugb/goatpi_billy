24206e5c7475a26fe5fbbe8d9ab2011bf5cf20f6
Douglas Bacon <douglasbacon@google.com>
2019-12-17 14:42:26 -0500

    adding getSuitableImageList

--- app.js
+++ app.js
@@ -13,29 +13,28 @@ const IMG_PATH = keys.IMG_PATH;
 const DATA_PATH = keys.DATA_PATH;
 
 
-const db = mysql.createConnection ({
+const db = mysql.createConnection({
   host: keys.host,
   user: keys.user,
   password: keys.password,
   database: keys.database,
 });
 
+// todo(dugb) handle the db server closing the connection.
 db.connect((err) => {
   if (err) {
-      throw err;
+    throw err;
   }
   console.log('Connected to database');
 });
 global.db = db;
-
-
 
 /** Responds with the latest image. */
 app.get('/image', (reg, res) => {
   const imageHandler = new ImageHandler();
   const imageList = imageHandler.getImageList(IMG_PATH);
   const sortedList = imageHandler.sortByModTime(imageList, IMG_PATH);
-  const mostRecentImage = imageHandler.getSuitableImage(sortedList, IMG_PATH);
+  const mostRecentImage = imageHandler.getMostRecentSuitableImage(sortedList, IMG_PATH);
   if (mostRecentImage) {
     res.sendFile(mostRecentImage);
   } else {
@@ -43,6 +42,13 @@ app.get('/image', (reg, res) => {
     res.send('error - no image found')
   }
 });
+
+/** Responds with an array of images. */
+app.get('/imagelist', (req, res) => {
+  const imageHandler = new ImageHandler();
+  const imageList = imageHandler.getSuitableImageList(IMG_PATH);
+  res.send(imageList);
+})
 
 /** Responds with the latest data. */
 app.get('/data', (req, res) => {
@@ -56,11 +62,11 @@ app.get('/dbr', (req, res) => {
   let query = "SELECT datetime, temp, level FROM GoatData WHERE datetime = (SELECT max(datetime) FROM GoatData);";
   db.query(query, (err, result) => {
     if (err) {
-        console.log('error: ', err);
+      console.log('error: ', err);
     }
     console.log('results: ', result[0]);
     res.send(result[0])
-    });
+  });
 });
 
 // db select all test.
@@ -68,11 +74,11 @@ app.get('/dball', (req, res) => {
   let query = "SELECT * FROM GoatData;";
   db.query(query, (err, result) => {
     if (err) {
-        console.log('error: ', err);
+      console.log('error: ', err);
     }
     console.log('results: ', result);
     res.send(result)
-    });
+  });
 });
 
 // db WRITE query test.
@@ -80,14 +86,13 @@ app.get('/dbw', (req, res) => {
   let query = "INSERT INTO GoatData (temp, level) VALUES(19, 42);";
   db.query(query, (err, result) => {
     if (err) {
-        console.log('error: ', err);
+      console.log('error: ', err);
     }
     console.log('results: ', result[0]);
     res.send(result[0])
-    });
+  });
 });
 
 app.listen(PORT, () =>
   console.log(`goatpi_billy started on port ${PORT}!`)
 );
-
--- imageHandler.js
+++ imageHandler.js
@@ -1,43 +1,94 @@
 const sizeOf = require('image-size');
 const fs = require('fs');
 
-const MIN_FILESIZE = 1;  // bytes
-const MIN_IMAGEWIDTH = 800;  // pixels
-const MIN_IMAGEHEIGHT = 450;  // pixles
+const MIN_FILESIZE = 1; // bytes
+const MIN_IMAGEWIDTH = 800; // pixels
+const MIN_IMAGEHEIGHT = 450; // pixles
 
 
 module.exports = class ImageHandler {
   constructor() {}
 
-  getImageList(imagePath){
+  /** 
+   * Gets all files from a path.
+   * @param {string} imagePath  Abs path of the images.
+   * @return {Array<string>}  Array of file names.
+   */
+  getImageList(imagePath) {
     return fs.readdirSync(imagePath).reverse();
   }
 
-  /**
-   * @param {Array<string>} imageList Array of image file names.
+  /** 
+   * Returns an array of filenames of suitable images.
+   * 
+   * I want to use this for a slideshow of all images for a date. Subject to
+   * change but I'm thinking the client will request a list of filenames
+   * (filenames only, I don't want the client to know anything about the 
+   * backends directory structure),  the client will then set the image url
+   * to something like:
+   *  https://goatpi.com/getImageUrl?image=filename&date=20191218
+   * then the get request handler will serve back the requested image.
+   * 
+   * // todo(dugb) need to have a source images seperated into directories by 
+   *    date.
+   * 
    * @param {string} imagePath Absolute path of the images.
-   * @return {string} Absolute path of an image.
+   * @return {Array<string>}  Array of file names
    */
-  getSuitableImage(imageList, imagePath){
-    let suitableImage;
-    for (const img of imageList) {
+  getSuitableImageList(imagePath) {
+    const imageList = this.getImageList(imagePath);
+    const sortedImageList = this.sortByModTime(imageList, imagePath);
+    let suitableImageList = [];
+    for (const img of sortedImageList) {
       const image = imagePath + img.name;
-      const stats = fs.statSync(image);
-      if (stats.size >= MIN_FILESIZE) {
-        const dimensions = sizeOf(image);
-        if (dimensions.width >= MIN_IMAGEWIDTH && dimensions.height >= MIN_IMAGEHEIGHT) {
-          suitableImage = image;
-          break;
-        }
+      if (this.isSuitableImage(image)) {
+        suitableImageList.push(img.name)
       }
     }
-    return suitableImage || undefined;
+    return suitableImageList;
   }
+
+  /** 
+   * Checks if an image is suitable for display.
+   * checks file size, width and height
+   * @param {string} image Absolute path and file name of the image.
+   * @returns {boolean}  True if suitable.
+   */
+  isSuitableImage(image) {
+    const stats = fs.statSync(image);
+    if (stats.size < MIN_FILESIZE) {
+      return false;
+    }
+    const dimensions = sizeOf(image);
+    if (dimensions.width < MIN_IMAGEWIDTH && dimensions.height < MIN_IMAGEHEIGHT) {
+      return false;
+    }
+    return true;
+  }
+
+  /**
+   * Returns the first suitable image from an array of images.
+   * For our use-case the array of images has been presorted by modified-time.
+   * 
+   * @param {Array<string>} imageList Array of image file names.
+   * @param {string} imagePath Absolute path of the images.
+   * @return {string | undefined} Absolute path of the suitable image or undefined if a suitable image was not found.
+   */
+  getMostRecentSuitableImage(imageList, imagePath) {
+    for (const img of imageList) {
+      const image = imagePath + img.name;
+      if (this.isSuitableImage(image)) {
+        return image;
+      }
+    }
+    return undefined;
+  }
+
   /**  
-    * @param {Array<string>} imageList Array of image file names.
-    * @param {string} imagePath Absolute path of the images.
-    * @return {Array<Object>}  Array of image file names and modified times, sorted by modified time.
-    */
+   * @param {Array<string>} imageList Array of image file names.
+   * @param {string} imagePath Absolute path of the images.
+   * @return {Array<Object>}  Array of image file names and modified times, sorted by modified-time.
+   */
   sortByModTime(imageList, imagePath) {
     let sortedlist = [];
     for (const img of imageList) {

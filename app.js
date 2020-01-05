b874a8d9e3e2d4884bd4e1bbffddc18bbee928cc
dugb <dugbdev@gmail.com>
2020-01-05 12:23:51 -0500

    moving Handlers into Routes and Controllers

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 
-// Routes.
+// Routes
 const imageRoutes = require('./routes/imageRoutes');
 app.use('/images', imageRoutes);
 const dataRoutes = require('./routes/dataRoutes');
@@ -11,17 +11,6 @@ app.use('/data', dataRoutes);
 
 const keys = require('./keys');
 const PORT = keys.PORT;
-const DATA_PATH = keys.DATA_PATH;
-
-
-
-
-/** Responds with the latest data. */
-app.get('/data', (req, res) => {
-  const dataHandler = new DataHandler();
-  const data = dataHandler.jsonReader(DATA_PATH + 'data.json')
-  res.status(200).json(data)
-});
 
 app.listen(PORT, () =>
   console.log(`goatpi_billy started on port ${PORT}!`)
